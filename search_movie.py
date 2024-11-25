dd0f5933701d7b7ded71660130830b89b67c1faf
Sourav Modak <official.srv.modak1@gmail.com>
2024-11-24 23:13:45 -0500

    removed API key

--- search_movie.py
+++ search_movie.py
@@ -1,14 +1,18 @@
 import requests
+import json
 
-
-
-# Usage example
-api_key = 'AIzaSyD2XfbI3qgPXme7Hr-BaiFh4OOqozkqx1w'  # Replace with your API key
-cse_id = 'c32ee9c562abe4e76'  # Replace with your Custom Search Engine ID
+# Load API key and CSE ID from the external JSON file
+def load_config():
+    with open('config.json') as config_file:
+        config = json.load(config_file)
+    return config['api_key'], config['cse_id']
 
 def get_movie_trailer(movie_name):
+    # Load the API key and CSE ID
+    api_key, cse_id = load_config()
+
     search_query = f"{movie_name} trailer site:youtube.com"
-    print("Search for movie : "+movie_name)
+    print("Search for movie: " + movie_name)
     url = f"https://www.googleapis.com/customsearch/v1?q={search_query}&key={api_key}&cx={cse_id}"
 
     response = requests.get(url)
@@ -21,7 +25,3 @@ def get_movie_trailer(movie_name):
             if "youtube.com" in link:
                 return link
     return "Trailer not found."
-
-# movie_name = input("Enter the movie name: ")
-# trailer_link = get_movie_trailer(movie_name)
-# print(f"Trailer link: {trailer_link}")
